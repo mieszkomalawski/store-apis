d2a387720520b9366baf88bb2e9a1288f4b24483
Mieszko Malawski <mmalawski@gog.com>
2018-04-18 08:35:03 +0200

    pact verfify config

--- tests/Pact/VerifyPactTest.php
+++ tests/Pact/VerifyPactTest.php
@@ -12,20 +12,25 @@ namespace Tests\Pact;
 use GuzzleHttp\Psr7\Uri;
 use PhpPact\Standalone\ProviderVerifier\Model\VerifierConfig;
 use PhpPact\Standalone\ProviderVerifier\Verifier;
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Yaml\Yaml;
 
-class VerifyPactTest extends KernelTestCase
+class VerifyPactTest extends TestCase
 {
     public function testVerifyAllPact()
     {
+        $config = Yaml::parse(file_get_contents(__DIR__ . '/../../app/config/parameters.yml'));
+        $brokerUri = $config['paremeters']['broker_uri'];
+        $providerUri = $config['paremeters']['provider_uri'];
         $config = new VerifierConfig();
         $config
             ->setProviderName('SomeProvider') // Providers name to fetch.
             ->setProviderVersion('1.0.0') // Providers version.
            // ->setProviderBaseUrl(new Uri('http://192.168.99.100:82')) // URL of the Provider.
            // ->setBrokerUri(new Uri('http://192.168.99.100:81')) // URL of the Pact Broker to publish results.
-                ->setProviderBaseUrl(new Uri('http://127.0.0.1:82'))
-                ->setBrokerUri(new Uri('http://ec2-52-14-29-29.us-east-2.compute.amazonaws.com/'))
+                ->setProviderBaseUrl(new Uri($providerUri))
+                ->setBrokerUri(new Uri($brokerUri))
                 ->setVerbose(true)
             ->setPublishResults(true); // Flag the verifier service to publish the results to the Pact Broker.
 
